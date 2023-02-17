1eb4959a268ab908527634ccc42e3022397aff1f
Andreas Rasmussen <xandreas@live.dk>
2023-02-17 12:50:23 +0100

    Add project files.

---
+++ C-Sharp/AccountCreation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Deployment.Internal;
+
+namespace C_Sharp
+{
+    public class AccountCreation
+    {
+        public StringBuilder createAccount(string username, string password)
+        {
+            // Instantiate an object of the StringBuilder class
+            StringBuilder errorMessage = new StringBuilder();
+
+            //
+            bool hasLowerCase = Regex.IsMatch(password, @"[a-z]");
+            bool hasNumber = Regex.IsMatch(password, @"\d");
+            bool hasSpecialCharacter = Regex.IsMatch(password, @"[^a-zA-Z0-9]");
+            bool hasUpperCase = Regex.IsMatch(password, @"^(?=.*[A-Z]).+$");
+
+            if (password.Length < 8)
+            {
+                errorMessage.Append("The password must be at least 8 characters long.\n");
+            }
+            if (!hasUpperCase)
+            {
+                errorMessage.Append("The password must contain at least one uppercase letter.\n");
+            }
+            if (!hasLowerCase)
+            {
+                errorMessage.Append("The password must contain at least one lowercase letter.\n");
+            }
+            if (!hasNumber)
+            {
+                errorMessage.Append("The password must contain at least one number.\n");
+            }
+            if (!hasSpecialCharacter)
+            {
+                errorMessage.Append("The password must contain at least one special character.\n");
+            }
+            if (errorMessage.Length == 0)
+            {
+                errorMessage.Append("The password meets all requirements.");
+                // create the user in here
+            }
+            else
+            {
+                score++;
+            }
+            return errorMessage;
+        }
+    }
+}
---
+++ C-Sharp/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.WriteLine("Enter a command:");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Enter a username: ");
+                        string username = Console.ReadLine();
+                        Console.Write("Enter a password: ");
+                        string password = Console.ReadLine();
+
+                        // Instantiate an object of the AccounCreation class
+                        AccountCreation account = new AccountCreation();
+
+                        // Call the CreateAccount method and store the result in a StringBuilder object
+                        StringBuilder errorMessage = account.createAccount(username, password);
+
+                        if (errorMessage.Length >= 1)
+                        {
+                            Console.WriteLine("Error creating account:");
+                            Console.WriteLine(errorMessage.ToString());
+                        }
+                        else if (errorMessage.Length == 0)
+                        {
+                            Console.WriteLine("Account created successfully!");
+                        }
+                        break;
+                    case "2":
+                        break;
+                    case "exit":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid command");
+                        break;
+                }
+            }
+        }
+    }
+}
