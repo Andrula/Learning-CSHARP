bff42e9231ce0ad3b2eee2e547bca40e22afaa88
Andreas Rasmussen <xandreas@live.dk>
2023-02-28 13:02:50 +0100

    Added parameters practice

---
+++ Company/Employee.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace Company
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }   
+        public string email { get; set; }   
+        public int hoursWorked { get; set; }
+        public DateTime birthday { get; set; }  
+
+        public void DoWork()
+        {
+            hoursWorked++;
+            Console.WriteLine($"{firstName} {lastName} has worked {hoursWorked} hours this month.");
+        }
+
+        public void DoWork(int numbersOfHoursWorked)
+        {
+            hoursWorked += numbersOfHoursWorked;
+            Console.WriteLine($"{firstName} {lastName} has worked {hoursWorked} hour(s).");
+        }
+
+        public void CalculateSalary (int numbersOfHoursWorked, int hourlySalary = 160)
+        {
+            double monthlySalary = (numbersOfHoursWorked * hourlySalary);
+            Console.WriteLine($"{firstName}'s salary this month is {monthlySalary} because she worked {numbersOfHoursWorked}");
+        }
+
+        public void Bonus(int numbersOfHoursWorked,int monthlySalary,int bonus = 500)
+        {
+            if (numbersOfHoursWorked > 10)
+            {
+                int addedBonus = (monthlySalary + bonus);
+                Console.WriteLine($"Because {firstName} worked more than 10 hours, she gets a bonus of {bonus}");
+            }
+        }
+    }
+}
---
+++ Company/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employeeOne = new Employee();
+            employeeOne.firstName = "Bethany";
+            employeeOne.lastName = "Adams";
+            employeeOne.email = "Bethany@adams.com";
+            employeeOne.DoWork(13);
+            employeeOne.CalculateSalary(13);
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
